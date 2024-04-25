ba1200b26739d53312d23da3ac72d2f8872e17d5
Colin Sutton <ctracysutton@gmail.com>
2024-04-25 01:38:55 -0700

    Made a logger

--- bot.py
+++ bot.py
@@ -1,9 +1,19 @@
 from discord.ext import commands
 import discord
+import settings
 
-BOT_TOKEN = 'MTIzMjkwODk2MDAxNzY3ODQxOA.GmyFAY.WQTYsHCbVyoTYhV2MQqPDoBvGvE8xoS3WbPB_E'
+logger = settings.logging.getLogger("bot")
+
 CHANNEL_ID = 1232929274571526174
 
-bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+def main():
+    bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
-bot.run(BOT_TOKEN)
+    @bot.event
+    async def on_ready():
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+
+if __name__ == "__main__":
+    main()
