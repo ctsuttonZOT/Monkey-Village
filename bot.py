5c330001c04d10d0624f22c42fccf86dfac355c7
Colin Sutton <ctsutton@uci.edu>
2024-07-21 11:46:06 -0700

    Made changes to the on_ready event which now starts the logger and creates a global instance of the Database class to be used in various commands. Created two new commands: register and get_monkey_money. The register command allows users to pass their Ninja Kiwi OAK id and have it stored in the bot's database, this allows users to use commands (such as get_monkey_money) which will retrieve their user data using the provided OAK id. The get_monkey_money command retrieves a user's amount of monkey money on their BTD6 account, if no argument is given then the author's data is retrieved, but if a discord username is provided, and that user has registered, it retrieves that user's data.

--- bot.py
+++ bot.py
@@ -1,19 +1,46 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
 import settings
+from ninja_kiwi_api import NinjaKiwiApi
+from database import Database
 
 logger = settings.logging.getLogger("bot")
 
+# Hardcoded now for testing, will be changed when bot is ready.
 CHANNEL_ID = 1232929274571526174
 
 def main():
-    bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+    bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
     @bot.event
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+        global db
+        db = Database()
 
+
+    @bot.command()
+    async def register(ctx, oak):
+        db.add_user(str(ctx.author), oak)
+        await ctx.send(f"User '{str(ctx.author)}' successfully registered.")
+
+
+    @bot.command()
+    async def get_monkey_money(ctx, username = None):
+        try:
+            if username:
+                api = NinjaKiwiApi(db.retrieve_user_oak(username))
+                api.load_data()
+                await ctx.send(f"{username} has {api.monkey_money} Monkey Money!")
+            else:
+                api = NinjaKiwiApi(db.retrieve_user_oak(str(ctx.author)))
+                api.load_data()
+                await ctx.send(f"You have {api.monkey_money} Monkey Money!")
+        except TypeError:
+            await ctx.send("Data for this user cannot be retrieved.")
+    
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+
 
 if __name__ == "__main__":
     main()
