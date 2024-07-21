08d602b34ba6d10c42ca08ad41371910f0df4754
Colin Sutton <ctsutton@uci.edu>
2024-07-21 11:02:06 -0700

    Created a new module, database.py, which creates, modifies, and retrieves data from a sqllite database which stores user's discord usernames and Ninja Kiwi OAK id's; this is done in a Database class.

---
+++ database.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+class Database:
+    def __init__(self) -> None:
+        self.connection = sqlite3.connect('./user-oaks.db')
+
+        # If 'users' table already exists, pass. Else create table
+        if self.connection.execute(
+            '''
+            SELECT name
+            FROM sqlite_master
+            WHERE type = 'table' AND name = 'users';
+            ''').fetchall():
+            pass
+        else:
+            self.connection.execute(
+                '''
+                CREATE TABLE users(
+                    user_name TEXT PRIMARY KEY,
+                    user_oak TEXT
+                )
+                ''')
+            self.connection.commit()
+    
+    def add_user(self, username: str, user_oak: str) -> None:
+        self.connection.execute(
+            f'''
+            INSERT INTO users (user_name, user_oak)
+            VALUES (?, ?)
+            ''',
+            (username, user_oak))
+        self.connection.commit()
+    
+    def retrieve_user_oak(self, username: str) -> str:
+        oak = self.connection.execute(
+            f'''
+            SELECT user_oak
+            FROM users
+            WHERE user_name = ?
+            ''', (username,))
+        return oak.fetchone()[0]
